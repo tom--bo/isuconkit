c67233f54c30662bb4dac4c8ff7fb7822dd19ead
tom--bo <otsuka.t.2013@gmail.com>
2015-09-27 00:04:18 +0900

    log_lotate by bench

---
+++ pre_bench.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+HOME_DIR=/home/bench_logs
+TIME=`date '+%R'`
+
+mkdir -p $HOME_DIR/$TIME/nginx
+mkdir -p $HOME_DIR/$TIME/apache
+mkdir -p $HOME_DIR/$TIME/mysql
+
+
+##  nginx  ##
+mv /var/log/nginx/access.log $HOME_DIR/$TIME/nginx
+mv /var/log/nginx/error.log $HOME_DIR/$TIME/nginx
+touch /var/log/nginx/access.log
+touch /var/log/nginx/error.log
+chmod 777 /var/log/nginx/access.log
+chmod 777 /var/log/nginx/error.log
+
+
+##  apache  ##
+# mv /var/log/httpd/access.log $HOME_DIR/$TIME/apache
+# mv /var/log/httpd/error.log $HOME_DIR/$TIME/apache
+# touch /var/log/httpd/access.log 
+# touch /var/log/httpd/error.log 
+# chmod 777 /var/log/httpd/access.log 
+# chmod 777 /var/log/httpd/error.log 
+
+
+##  mysqld  ##
+mv /var/log/mysqld.log $HOME_DIR/$TIME/mysql
+mv /var/log/mysql-slow.log $HOME_DIR/$TIME/mysql
+touch /var/log/mysqld.log
+touch /var/log/mysql-slow.log
+chmod 777 /var/log/mysqld.log
+chmod 777 /var/log/mysql-slow.log
+
+
+
