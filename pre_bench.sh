7e346532f6cf8c70b3f4c5769aa36ff2cdac5c8d
tom--bo <otsuka.t.2013@gmail.com>
2015-09-27 00:19:05 +0900

    fix

--- pre_bench.sh
+++ pre_bench.sh
@@ -2,14 +2,14 @@
 HOME_DIR=/home/bench_logs
 TIME=`date '+%R'`
 
-mkdir -p $HOME_DIR/$TIME/nginx
-mkdir -p $HOME_DIR/$TIME/apache
-mkdir -p $HOME_DIR/$TIME/mysql
+mkdir -p $HOME_DIR/log_$TIME/nginx
+mkdir -p $HOME_DIR/log_$TIME/apache
+mkdir -p $HOME_DIR/log_$TIME/mysql
 
 
 ##  nginx  ##
-mv /var/log/nginx/access.log $HOME_DIR/$TIME/nginx
-mv /var/log/nginx/error.log $HOME_DIR/$TIME/nginx
+mv /var/log/nginx/access.log $HOME_DIR/log_$TIME/nginx
+mv /var/log/nginx/error.log $HOME_DIR/log_$TIME/nginx
 touch /var/log/nginx/access.log
 touch /var/log/nginx/error.log
 chmod 777 /var/log/nginx/access.log
@@ -17,21 +17,20 @@ chmod 777 /var/log/nginx/error.log
 
 
 ##  apache  ##
-# mv /var/log/httpd/access.log $HOME_DIR/$TIME/apache
-# mv /var/log/httpd/error.log $HOME_DIR/$TIME/apache
-# touch /var/log/httpd/access.log 
-# touch /var/log/httpd/error.log 
-# chmod 777 /var/log/httpd/access.log 
-# chmod 777 /var/log/httpd/error.log 
+# mv /var/log/httpd/access.log $HOME_DIR/log_$TIME/apache
+# mv /var/log/httpd/error.log $HOME_DIR/log_$TIME/apache
+# touch /var/log/httpd/access.log
+# touch /var/log/httpd/error.log
+# chmod 777 /var/log/httpd/access.log
+# chmod 777 /var/log/httpd/error.log
 
 
 ##  mysqld  ##
-mv /var/log/mysqld.log $HOME_DIR/$TIME/mysql
-mv /var/log/mysql-slow.log $HOME_DIR/$TIME/mysql
+mv /var/log/mysqld.log $HOME_DIR/log_$TIME/mysql
+mv /var/log/mysql-slow.log $HOME_DIR/log_$TIME/mysql
 touch /var/log/mysqld.log
 touch /var/log/mysql-slow.log
 chmod 777 /var/log/mysqld.log
 chmod 777 /var/log/mysql-slow.log
-
 
 
